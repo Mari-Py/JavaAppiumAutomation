ec28b6c1cca0b7fefbec980c44161f1eceb66366
Mari-Py <beretta07@bk.ru>
2022-07-29 20:19:36 +0300

    Add waiting

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,4 +1,3 @@
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -6,9 +5,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 import java.time.Duration;
+
 
 public class FirstTest {
     public AppiumDriver driver;
@@ -26,6 +29,7 @@ public class FirstTest {
 
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
     }
 
     @After
@@ -38,6 +42,22 @@ public class FirstTest {
     public void firstTest()
     {
         driver.findElement(By.id("org.wikipedia:id/search_container")).click();
-        //System.out.println("First test run");
+        driver.findElement(By.id("org.wikipedia:id/search_src_text")).sendKeys("Java");
+        driver.findElement(By.xpath("//*resource_id='org.wikipedia:id/page_list_item_description']" +
+                "//*[@text='язык программирования']"));
+
+
+        driver.findElement(By.xpath("//*resource_id='org." +
+                "wikipedia:id/page_list_item_description']//*[@text='язык программирования']"));
     }
+
+    private WebElement waitForElementByXpath(String xpath, String error_messege, long timeout)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.withMessage(error_messege + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+
 }
